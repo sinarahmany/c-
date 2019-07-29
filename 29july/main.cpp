5bd6e18d14b28849098a6d8d367d27f22d6ec238
sinarah22 <sinarahmannejad@itdcanada.ca>
2019-07-29 10:07:22 -0700

    signed and unsigned value

--- 29july/main.cpp
+++ 29july/main.cpp
@@ -9,12 +9,12 @@ int main()
     std::cout << "Hello world! \n" ;
     std::cout << "Hello world! \x0A" ;
 
-    std::cout << "Gice me a number \n" ;
+    std::cout << "Give me a number \n" ;
     int number;
     std::cin >> number;
     std::cout << "you typed " << number << "!" << std::endl;
 
-    std::cout << "Gice me one more number \n" ;
+    std::cout << "Give me one more number \n" ;
     int number2;
     std::cin >> number2;
     std::cout << "Your numbers are "<< number << "," << number2 << std::endl;
@@ -23,5 +23,11 @@ int main()
     std::cout << "the average of those numbers is " << (double)(number+number2)/2 <<std::endl;
     //it`s more secure static casting
     std::cout << "the average of those numbers is " << (static_cast<double>(number+number2)/2) <<std::endl;
+
+    //signed and unsigned
+    //unsigned values are just for positive values
+    unsigned int max = 1;
+
+
     return 0;
 }
