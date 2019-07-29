f685eeff7d0ad3dc237d685ccc1d2860b260b827
sinarah22 <sinarahmannejad@itdcanada.ca>
2019-07-29 09:34:52 -0700

    addition added

--- 29july/main.cpp
+++ 29july/main.cpp
@@ -9,9 +9,15 @@ int main()
     std::cout << "Hello world! \n" ;
     std::cout << "Hello world! \x0A" ;
 
-
+    std::cout << "Gice me a number \n" ;
     int number;
-    std::cin>> number;
+    std::cin >> number;
     std::cout << "you typed " << number << "!" << std::endl;
+
+    std::cout << "Gice me one more number \n" ;
+    int number2;
+    std::cin >> number2;
+    std::cout << "Your numbers are "<< number << "," << number2 << std::endl;
+    std::cout << "the addition of those numbers are " << number+number2 <<std::endl;
     return 0;
 }
