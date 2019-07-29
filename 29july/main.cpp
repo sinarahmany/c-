7bc56c5e0dc8d7853173f4252950767b6dd5c7a5
sinarah22 <sinarahmannejad@itdcanada.ca>
2019-07-29 11:52:50 -0700

    menu

--- 29july/main.cpp
+++ 29july/main.cpp
@@ -7,23 +7,25 @@ using namespace std;
 
 int main()
 {
-    somedatatypes();
-//bool flag=true;
-//    while(flag=true){
-//        std::cout << "1-push a value to stract | 2-pop a value from stract | 3-print the value in stract" ;
-//        int choice;
-//        switch(choice):
-//        case 1 :
-//            break;
-//        case 2 :
-//            break;
-//        case 3 :
-//            break;
-//        default:
-//            std::cout << "Not In The Menu \n" ;
-//            flag=false;
-//
-//    }
+   // somedatatypes();
+bool flag=true;
+    while(flag=true){
+        std::cout << "1-push a value to stack | 2-pop a value from stack | 3-print the value in stack \n" ;
+        int choice;
+        std::cin >> choice;
+        switch(choice){
+        case 1 :
+            break;
+        case 2 :
+            break;
+        case 3 :
+            break;
+        default:
+            std::cout << "Not In The Menu \n" ;
+            flag=false;
+
+        }
+    }
 
 
 
