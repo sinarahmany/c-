6af548b8d3e8b679beffee2f0edf88d148b2241f
sinarah22 <sinarahmannejad@itdcanada.ca>
2019-08-01 09:15:20 -0700

    29 aug

--- 1 aug/main.cpp
+++ 1 aug/main.cpp
@@ -2,8 +2,29 @@
 
 using namespace std;
 
+int modify(int a,int &b,int &c ){
+a+=1;
+b+=2;
+c+=3;
+return a+b+c;
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    int x = 5;
+    int y = 4;
+    std::cout << add(x,y) <<std::endl;
+
+    int i,j,k;
+    i=5 ; //this will be pass by value
+    j=4 ; //this will be pass by ref
+    k=2 ;
+    //pass bt reference will check the address in the RAM
+    std::cout <<modify(i,j,k) <<std:: endl;
+    //the value of i is not modified with the pass by val function
+    std::cout << " value of i is :" << i << std::endl;//4
+    //the value of j is modified with the pass ny reference function
+    std::cout << "value of j is : " << j << std::endl; //4
+
     return 0;
 }
