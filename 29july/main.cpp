b9d723f623fd01395e955e9e19246da13e9e7b55
sinarah22 <sinarahmannejad@itdcanada.ca>
2019-08-01 09:06:59 -0700

    new project empty

--- 29july/main.cpp
+++ 29july/main.cpp
@@ -13,7 +13,6 @@ string bla_bla(std::string strInput);
 
 int main()
 {
-
     string beeeep;
     std::cin >> beeeep ;
     std::cout << bla_bla(beeeep);
@@ -26,7 +25,7 @@ int main()
 
 string bla_bla(std::string strInput){
     int n = strInput.length();
-string arru="";
+    string arru="";
     for(int i = n-1;i>=0;i --){
         arru+= strInput[i];
 
