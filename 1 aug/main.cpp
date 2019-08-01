d20cdf48cbc833eb5f0f5de72b07d814a6e7f88d
sinarah22 <sinarahmannejad@itdcanada.ca>
2019-08-01 09:24:10 -0700

    29 aug fixed bug

--- 1 aug/main.cpp
+++ 1 aug/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 
 using namespace std;
-
+int add (int a,int b){
+    return a+b; }
 int modify(int a,int &b,int &c ){
 a+=1;
 b+=2;
@@ -13,16 +14,16 @@ int main()
 {
     int x = 5;
     int y = 4;
-    std::cout << add(x,y) <<std::endl;
+   // std::cout << add(x,y) <<std::endl;
 
     int i,j,k;
     i=5 ; //this will be pass by value
     j=4 ; //this will be pass by ref
     k=2 ;
     //pass bt reference will check the address in the RAM
-    std::cout <<modify(i,j,k) <<std:: endl;
+    std::cout << modify(i,j,k) <<std:: endl;
     //the value of i is not modified with the pass by val function
-    std::cout << " value of i is :" << i << std::endl;//4
+    std::cout << "value of i is :" << i << std::endl;//4
     //the value of j is modified with the pass ny reference function
     std::cout << "value of j is : " << j << std::endl; //4
 
