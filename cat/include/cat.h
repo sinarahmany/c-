28055f7804662eeb543c4b4125be000fe94e3ed2
sinarah22 <sinarahmannejad@itdcanada.ca>
2019-08-02 11:32:32 -0700

    oop

--- cat/include/cat.h
+++ cat/include/cat.h
@@ -4,13 +4,23 @@
 
 class cat
 {
-    public:
-        cat();
-        virtual ~cat();
+    std::string name;
+    std::string color;
+    bool single;
+    double weight;
+    int health;
 
-    protected:
 
-    private:
+    void jump(){
+    std::cout << "i jumped like a crazy Cat!!" << std::endl;
+    }
+
+    void sleep(){
+    std::cout <<"i`m sleeping call me later." << std::endl;
+    }
+    void eat(){
+    std::cout << "Hmmmm i like tuna" << std::endl;
+    }
 };
 
 #endif // CAT_H
