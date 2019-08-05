2db9192a441d9a7bf21891c4f63fa02ee960796e
sinarah22 <sinarahmannejad@itdcanada.ca>
2019-08-04 23:45:27 -0700

    thanks to google assignment done such a shamegit add .

--- assignmentc++/main.cpp
+++ assignmentc++/main.cpp
@@ -5,21 +5,29 @@ using namespace std;
 
 int main()
 {
+
     string a;
     std::cout << " Enter a Text or Numbers " << std::endl;
     std::cin >> a ;
     //std::cout << a.length() << std::endl;
-        for (int i=0;i<a.length()+1;i++){
-            for (int j=0;j<i;j++){
-                std::cout << "\t1" ;
-                std::cout << a[j];
-
-            }
-            std::cout << " " << std::endl;
-
-        }
-
-
+int i,j,k,l,n;
+n= a.length();
+for(i=1;i<=n;i++)
+{
+for(j=1;j<=n-i;j++)
+{
+cout<<" ";
+}
+for(k=1;k<=i;k++)
+{
+cout<<a[k-1];
+}
+for(l=i-1;l>=1;l--)
+{
+cout<<a[l-1];
+}
+cout<<"\n";
+}
 
 
     return 0;
