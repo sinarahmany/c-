1225e6c30a002c1314ccd4c41a46a19e1ba986c5
sinarah22 <sinarahmannejad@itdcanada.ca>
2019-08-12 09:57:33 -0700

    who is right

--- Streamcplus/main.cpp
+++ Streamcplus/main.cpp
@@ -7,19 +7,22 @@ using namespace std;
 int main()
 {
     std::ifstream Readobj;
+
+
     Readobj.open("main.txt");
-    std::string line;
-    int bestnumber;
-    double coffeePrice;
+
+
+    std::string Name;
+    int Age;
     bool isItTrue;
 
-    Readobj >> line >> bestnumber >> coffeePrice >> isItTrue;
-    std::cout << "Line is : "<<line <<std::endl;
-    std::cout <<"Best Number is : " <<bestnumber <<std::endl;
-    std::cout <<"Coffee price is : " << coffeePrice <<std::endl;
-    //bool alpha iin the line below changes the value of bool from 0 to true or false
-    std::cout << "isitTrue = "<< std::boolalpha <<isItTrue <<std::endl;
+    while(!Readobj.eof()){
+        Readobj >> Name >> Age >> isItTrue;
+        //bool alpha iin the line below changes the value of bool from 0 to true or false
+        std::cout << "name : "<<Name<<"| Age is :"<<Age <<" | That is : "<< std::boolalpha <<isItTrue <<std::endl;
 
+
+    }
 
     return 0;
 }
