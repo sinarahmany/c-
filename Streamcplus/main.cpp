68933c022aa33b7f11877f5450ca20794b239d0a
sinarah22 <sinarahmannejad@itdcanada.ca>
2019-08-12 09:32:41 -0700

    some new things added to stream

--- Streamcplus/main.cpp
+++ Streamcplus/main.cpp
@@ -9,7 +9,16 @@ int main()
     std::ifstream Readobj;
     Readobj.open("main.txt");
     std::string line;
-    Readobj >> line;
-    std::cout << line ;
+    int bestnumber;
+    double coffeePrice;
+    bool isItTrue;
+
+    Readobj >> line >> bestnumber >> coffeePrice >> isItTrue;
+    std::cout << "Line is : "<<line <<std::endl;
+    std::cout <<"Best Number is : " <<bestnumber <<std::endl;
+    std::cout <<"Coffee price is : " << coffeePrice <<std::endl;
+    std::cout << "isitTrue = "<< isItTrue <<std::endl;
+
+
     return 0;
 }
