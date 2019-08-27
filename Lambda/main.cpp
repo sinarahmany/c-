4df5797d46ea8c6485ee0aa7454e786ba89f9f96
sinarah22 <sinarahmannejad@gmail.com>
2019-08-27 09:52:44 -0700

    Limbda added

--- Lambda/main.cpp
+++ Lambda/main.cpp
@@ -20,5 +20,27 @@ int main()
 {
     sayGoodMorning("Sina",sayBomdia);
     sayGoodMorning("Taylor",saysobbekhyr);
-    return 0;
+
+    //Lambda c++ way
+    function <void()> TheNameOfTheFunction = [] () {
+        cout <<"this is how we create a Lambda expression c++ way" <<endl ;
+    };
+    TheNameOfTheFunction();
+
+    cout<<"***************************************"<<endl;
+    auto autoLambda= [](){
+        cout <<"I`m an auto Lambda c++ way"<<endl;
+    };
+    autoLambda();
+
+    cout<<"***************************************"<<endl;
+    //c way to create lambda
+   void(*cwayLambda) () = [](){
+        cout << "this is how we create a Lambda expression c way" <<endl;
+    };
+    cwayLambda();
+
+
+
+
 }
