e4204a0fa444813251a6779ecb843a27c7e86668
sinarah22 <sinarahmannejad@gmail.com>
2019-09-18 20:01:09 -0700

    assigment 11 compeleted

--- assignment 11/main.cpp
+++ assignment 11/main.cpp
@@ -1,5 +1,5 @@
 // Section 20
-// Challenge 2
+// Challenge 2 - Solution
 //  Lists
 
 #include <iostream>
@@ -11,6 +11,7 @@
 
 class Song {
     friend std::ostream &operator<<(std::ostream &os, const Song &s);
+
     std::string name;
     std::string artist;
     int rating;
@@ -18,12 +19,15 @@ public:
     Song() = default;
     Song(std::string name, std::string artist, int rating)
             : name{name}, artist{artist}, rating{rating} {}
+
     std::string get_name() const {
         return name;
     }
+
     std::string get_artist() const {
         return artist;
     }
+
     int get_rating() const {
         return rating;
     }
@@ -55,17 +59,16 @@ void display_menu() {
 }
 
 void play_current_song(const Song &song) {
-    // This function should display
-    // Playing: followed by the song that is playing
-
-    std::cout << "You implement this function"<< std::endl;
+    std::cout << "Playing: " << std::endl;
+    std::cout << song << std::endl;
 }
 
 void display_playlist(const std::list<Song> &playlist, const Song &current_song) {
-    // This function should display the current playlist
-    // and then the current song playing.
-
-    std::cout << "You implement this function" << std::endl;
+    for (const Song &song: playlist) {
+        std::cout << song << std::endl;
+    }
+    std::cout << "Current song: " << std::endl;
+    std::cout << current_song << std::endl;
 }
 
 int main() {
@@ -80,9 +83,58 @@ int main() {
     };
 
     std::list<Song>::iterator current_song = playlist.begin();
+    display_playlist(playlist, *current_song);
 
-    std::cout << "To be implemented" << std::endl;
-    // Your program logic goes here
+    char selection{};
+    do {
+        display_menu();
+        std::cin >> selection;
+        selection = std::toupper(selection);
+        if (selection == 'F') {
+            std::cout << "Playing first song" << std::endl;
+            current_song = playlist.begin();
+            play_current_song(*current_song);
+        }
+        else if (selection == 'N') {
+            std::cout << "Playing next song" << std::endl;
+            current_song++;
+            if (current_song == playlist.end()) {
+                std::cout << "Wrapping to start of playlist" << std::endl;
+                current_song = playlist.begin();
+            }
+            play_current_song(*current_song);
+        }
+        else if (selection == 'P') {
+            std::cout << "Playing previous song" << std::endl;
+            if (current_song == playlist.begin()) {
+                std::cout << "Wrapping to end of playlist" << std::endl;
+                current_song = playlist.end();
+            }
+            current_song--;
+            play_current_song(*current_song);
+        } else if (selection == 'A') {
+            std::string name, artist;
+            int rating;
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+            std::cout << "Adding and playing new song" << std::endl;
+            std::cout << "Enter song name:";
+            getline(std::cin, name);
+            std::cout << "Enter song artist: ";
+            getline(std::cin, artist);
+            std::cout << "Enter your rating (1-5): ";
+            std::cin >> rating;
+            playlist.insert(current_song, Song{name, artist, rating});
+            current_song--;
+            play_current_song(*current_song);
+        } else if (selection == 'L') {
+            std::cout << std::endl;
+            display_playlist(playlist, *current_song);
+        } else if (selection == 'Q')
+            std::cout << "Quitting" << std::endl;
+        else
+            std::cout << "Illegal choice, try again...";
+    } while (selection != 'Q');
 
     std::cout << "Thanks for listening!" << std::endl;
     return 0;
--- set/main.cpp
+++ set/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <set>
+#include <unordered_set>
+
 using namespace std;
 template<typename T>
 void printSet(const std::set<T> &theSet){
@@ -8,10 +10,20 @@ void printSet(const std::set<T> &theSet){
     }
     cout<<"\n";
 }
+template<typename T>
+void printUnorderedSet(const std::unordered_set<T> &theSet){
+    for(auto &element : theSet){
+        cout<<element << " ";
+    }
+    cout<<"\n";
+}
 int main()
 {
     //numbers {1,2,3,4,4,5,5,5,5}; has the same cout
-    //set ignores duplicate
-    std::set<int> numbers {1,2,3,4,5};
+    //set ignores duplicate and sort them
+    std::set<int> numbers {1,2,3,4,5,9,14,10,7};
+    std::unordered_set<int> numbers2 {1,2,3,4,5,9,14,10,7};
     printSet(numbers);
+    printUnorderedSet(numbers2);
+
 }
